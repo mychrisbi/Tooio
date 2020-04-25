77bd490c60b6d055c89e0e56b49fc00899b1c537
Peter Blohm <peter.blohm@outlook.at>
2020-04-25 14:19:32 +0200

    Add Switch for rendering different views

--- client/src/components/ClassroomComponent.vue
+++ client/src/components/ClassroomComponent.vue
@@ -1,34 +1,34 @@
 <template>
-<v-container>
-            <v-card color="green" dark height=150px align-beginning>
-              <v-card-text>I am a blackboard because that's easier than a whole ass classroom.</v-card-text>
-            </v-card>
-            <v-row>
-              <v-col
-                v-for="n in 4"
-                :key="n"
-                cols="6"
-                md="3"
-                justify='end'
-              >
-                <v-btn @click="test(n)">
-                  Tisch {{n}}
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-container>
+  <v-container>
+    <v-card color="green" dark height="150px" align-beginning v-show="overview == true">
+      <v-card-text>I am a blackboard because that's easier than a whole ass classroom.</v-card-text>
+    </v-card>
+    <v-row>
+      <v-col v-for="n in 4" :key="n" cols="6" md="3" justify="end" v-show="overview == true">
+        <v-btn @click="test(n)">Tisch {{n}}</v-btn>
+      </v-col>
+    </v-row>
+  <DocumentComponent style="height: 100%" v-show="overview == false"/>
+  <v-switch v-model="overview" :label="`overview: ${overview.toString()}`"></v-switch>
+  </v-container>
+
 </template>
 
 <script>
-  export default {
-    name: "classRoomComponent",
-    data: () => ({
-    }),
-    methods:{
-      test: function (n) {
-        alert("Hi " + n);
-        //TODO connect to peers on that table
-      }
+import DocumentComponent from './DocumentComponent.vue';
+export default {
+  name: "classRoomComponent",
+  data: () => ({
+    overview: true
+  }),
+  methods: {
+    test: function(n) {
+      alert("Hi " + n);
+      //TODO connect to peers on that table
     }
+  },
+  components: {
+    DocumentComponent
   }
+};
 </script>
