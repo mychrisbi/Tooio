f04232b3f020ff8f7b9d15dca3247cdc00f7a901
Peter Blohm <35277333+Pietreus@users.noreply.github.com>
2020-04-25 12:30:53 +0200

    Vuetify ui (#1)
    * create ugly ui prototype
    
    * create components
    
    * Import components into App.vue
    
    * Add button functionality
    
    Co-authored-by: Benjamin Wieser <Benjamin.Wieser@cloudflight.io>

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <main>
+      <v-container fluid grid-list-md fill-height align-end>
+        <v-flex d-flex xs12 sm12 md6>
+          <DocumentComponent />
+        </v-flex>
+        <v-col>
+          <ClassRoomComponent />
+          <v-layout align-end>
+            <v-flex v-for="i in 3" :key="i" xs4>
+              <portraitComponent :url="`https://randomuser.me/api/portraits/men/${i + 20}.jpg`" />
+            </v-flex>
+          </v-layout>
+        </v-col>
+      </v-container>
+    </main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import DocumentComponent from './components/DocumentComponent.vue'
+  import ClassRoomComponent from './components/ClassroomComponent.vue'
+  import PortraitComponent from './components/PortraitComponent.vue'
+  export default {
+    data: () => ({
+      lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`
+    }),
+    components: {
+      DocumentComponent,
+      ClassRoomComponent,
+      PortraitComponent
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ client/src/components/PortraitComponent.vue
@@ -0,0 +1,11 @@
+<template>
+              <img :src="url" class="image" alt="lorem" width="100%"
+                height="100%">
+</template>
+
+<script>
+  export default {
+    name: "portraitComponent",
+    props: ['url']
+  }
+</script>
