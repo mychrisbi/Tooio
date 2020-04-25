7594f03c0625a4997994e2c362b727080f2e9687
Benjamin Wieser <Benjamin.Wieser@cloudflight.io>
2020-04-25 10:22:41 +0200

    create components

---
+++ client/src/components/ClassroomComponent.vue
@@ -0,0 +1,29 @@
+<template>
+<v-container>
+            <v-card color="green" dark height=150px align-beginning>
+              <v-card-text>I am a blackboard because that's easier than a whole ass classroom.</v-card-text>
+            </v-card>
+            <v-row>
+              <v-col
+                v-for="n in 4"
+                :key="n"
+                cols="6"
+                md="3"
+                justify='end'
+              >
+                <v-btn 
+                >
+                  Tisch {{n}}
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+</template>
+
+<script>
+  export default {
+    nam: "classRoomComponent",
+    data: () => ({
+    })
+  }
+</script>
---
+++ client/src/components/DocumentComponent.vue
@@ -0,0 +1,15 @@
+<template>
+          <v-card color="purple" dark style="height: 800px;">
+            <v-card-title primary class="title">Document</v-card-title>
+            <v-card-text>{{ lorem }}</v-card-text>
+          </v-card>
+</template>
+
+<script>
+  export default {
+    nam: "documentComponent",
+    data: () => ({
+      lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`
+    })
+  }
+</script>
